d0de26ffc4b41004f52e18c35633fe85379e45ad
Sacha <sacha.trouve0@gmail.com>
2026-09-04 16:38:13 +0200

    Update waybar script

--- config/waybar/scripts/toggle_effects.sh
+++ config/waybar/scripts/toggle_effects.sh
@@ -2,24 +2,19 @@
 
 STATE_FILE="$HOME/.cache/hypr_effects_state"
 
-# Lire l'état actuel, nettoyer tout caractère parasite
+# Read current state
 if [[ -f "$STATE_FILE" ]]; then
     STATE=$(<"$STATE_FILE")
-    # Supprimer espaces et retours à la ligne
     STATE=$(echo -n "$STATE" | tr -d '[:space:]')
 else
     STATE="on"
 fi
 
-# Basculer l'état
+# Toggle state
 if [[ "$STATE" == "on" ]]; then
-    hyprctl keyword animations:enabled false
-    hyprctl keyword decoration:blur:enabled false
-    hyprctl keyword decoration:rounding 0
+    hyprctl eval "hl.config({ animations = { enabled = false }, decoration = { blur = { enabled = false }, rounding = 0 } })"
     echo -n "off" >"$STATE_FILE"
 else
-    hyprctl keyword animations:enabled true
-    hyprctl keyword decoration:blur:enabled true
-    hyprctl keyword decoration:rounding 20
+    hyprctl eval "hl.config({ animations = { enabled = true }, decoration = { blur = { enabled = true }, rounding = 20 } })"
     echo -n "on" >"$STATE_FILE"
 fi
